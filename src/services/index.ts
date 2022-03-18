42ab9ecf2d23d73361e6e7f454c34c9f8abe3631
owlran <owlran1088@gmail.com>
2022-03-19 01:28:29 +0800

    add api services

---
+++ src/services/index.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosResponse } from "axios";
+
+// create an axios instance, with base url: https://avl-frontend-exam.herokuapp.com/
+export const api = axios.create({
+  baseURL: "https://avl-frontend-exam.herokuapp.com/",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// getFriends with params page and pageSize, endpoint is /api/users/friends
+// ex: https://avl-frontend-exam.herokuapp.com/api/users/friends?page=1&pageSize=10
+export const getFriends = (
+  page: number,
+  pageSize: number
+): Promise<AxiosResponse<any>> => {
+  return api.get(`/api/users/friends?page=${page}&pageSize=${pageSize}`);
+};
+
+// getUser with params page, pageSize and keyword(optional), endpoint is /api/users/all
+// without keyword ex: https://avl-frontend-exam.herokuapp.com/api/users/all?page=1&pageSize=10
+// with keyword ex: https://avl-frontend-exam.herokuapp.com/api/users/all?page=1&pageSize=10&keyword=a
+export const getUsers = (
+  page: number,
+  pageSize: number,
+  keyword?: string
+): Promise<AxiosResponse<any>> => {
+  return api.get(
+    `/api/users/all?page=${page}&pageSize=${pageSize}${
+      keyword ? `&keyword=${keyword}` : ""
+    }`
+  );
+};
+
+// getTags with url https://avl-frontend-exam.herokuapp.com/api/tags
+export const getTags = (): Promise<AxiosResponse<any>> => {
+  return api.get(`/api/tags`);
+};
